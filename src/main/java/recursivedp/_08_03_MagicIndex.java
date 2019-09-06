b6601a6ee67f14478c812d042b0a9e95e573f4a2
Sachin <sachin20@uw.edu>
2019-09-05 18:50:49 -0700

    finished recursivedp q3

--- src/main/java/recursivedp/_08_03_MagicIndex.java
+++ src/main/java/recursivedp/_08_03_MagicIndex.java
@@ -7,6 +7,33 @@ package recursivedp;
 class _08_03_MagicIndex {
 
     int findMagicIndex(int[] arr) {
-        throw new UnsupportedOperationException();
+        return helper(arr, 0, arr.length - 1);
+    }
+
+    int helper(int[] arr, int beg, int end) {
+
+        if(end - beg < 3) {
+            for(int i = beg; i <= end; i++) {
+                if(arr[i] == i) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        int mid = beg + (end - beg) / 2;
+        if(arr[beg] == beg) {
+            return beg;
+        }
+        if(arr[end] == end) {
+            return end;
+        }
+
+        if(arr[beg] > end || arr[end] < beg) {
+            return -1;
+        } else {
+            return Math.max(helper(arr, beg, mid), helper(arr, mid + 1, end));
+        }
+
     }
 }
